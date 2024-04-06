8649381611d0a5ed05c3609f115a76f251755f77
Juan Pablo Mosquera <37482029+jpmosquerac@users.noreply.github.com>
2024-04-06 00:50:11 -0500

    testing

--- modules/deployments/deploy_grpc.tf
+++ modules/deployments/deploy_grpc.tf
@@ -29,20 +29,20 @@ resource "kubernetes_deployment" "grpc_deployment" {
           port {
             container_port = 50051
           }
-          command = [
-            "git clone -b v1.62.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc",
-            "cd grpc/examples/python/route_guide",
-            "python -m grpc_tools.protoc -I../../protos --python_out=. --pyi_out=. --grpc_python_out=. ../../protos/route_guide.proto",
-            "python -m grpc_tools.protoc -Igrpc/example/custom/path=../../protos --python_out=. --grpc_python_out=. ../../protos/route_guide.proto",
-            "python route_guide_server.py"
-          ]
+          # command = [
+          #   "git clone -b v1.62.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc",
+          #   "cd grpc/examples/python/route_guide",
+          #   "python -m grpc_tools.protoc -I../../protos --python_out=. --pyi_out=. --grpc_python_out=. ../../protos/route_guide.proto",
+          #   "python -m grpc_tools.protoc -Igrpc/example/custom/path=../../protos --python_out=. --grpc_python_out=. ../../protos/route_guide.proto",
+          #   "python route_guide_server.py"
+          # ]
         }
       }
     }
   }
 }
 
-resource "kubernetes_service" "test" {
+resource "kubernetes_service" "py-grpc-service" {
   metadata {
     name      = "py-grpc"
     namespace = kubernetes_namespace.grpc_namespace.metadata.0.name
