014c92ab7d49904df32ae370c8caac77306ca2b0
Juan Pablo Mosquera <37482029+jpmosquerac@users.noreply.github.com>
2024-04-06 20:12:05 -0500

    commenting deployment provider

--- modules/deployments/provider.tf
+++ modules/deployments/provider.tf
@@ -1,5 +1,5 @@
-provider "kubernetes" {
-  host                   = var.aws_eks_cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster_auth.token
-  cluster_ca_certificate = base64decode(var.aws_eks_cluster.certificate_authority.0.data)
-}
+# provider "kubernetes" {
+#   host                   = var.aws_eks_cluster.endpoint
+#   token                  = data.aws_eks_cluster_auth.cluster_auth.token
+#   cluster_ca_certificate = base64decode(var.aws_eks_cluster.certificate_authority.0.data)
+# }
